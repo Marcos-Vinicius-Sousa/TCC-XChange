aedf933788685bb48c3ee718d11e01a0589b9357
Marcos-Barry <55248963+Marcos-Barry@users.noreply.github.com>
2021-08-02 23:11:17 -0300

    finalizando a interface e o metodo da ligação

--- x_change/lib/views/DetalhesAnuncios.dart
+++ x_change/lib/views/DetalhesAnuncios.dart
@@ -1,5 +1,7 @@
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:x_change/model/Anuncio.dart';
 
 class DetalhesAnuncios extends StatefulWidget {
@@ -31,6 +33,15 @@ class _DetalhesAnunciosState extends State<DetalhesAnuncios> {
     }).toList();
   }
 
+  _ligarTelefone(String telefone) async {
+
+    if(await canLaunch("tel: $telefone")){
+      await launch("tel: $telefone");
+    }else{
+      print("Não pode fazer essa ligação");
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -59,8 +70,95 @@ class _DetalhesAnunciosState extends State<DetalhesAnuncios> {
                 autoplay: false,
                 dotIncreasedColor: Colors.indigo,
               ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                      "R\$ ${_anuncio.preco}",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigo
+                    ),
+                  ),
+                  Text(
+                    "${_anuncio.titulo}",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Divider(),
+                  ),
+                  Text(
+                    "Descrição",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    "${_anuncio.descricao}",
+                    style: TextStyle(
+                      fontSize: 18
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Divider(),
+                  ),
+                  Text(
+                    "Contato",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 40),
+                    child: Text(
+                      "${_anuncio.telefone}",
+                      style: TextStyle(
+                          fontSize: 18
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
-          ],)
+          ],),
+
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 16,
+            child: GestureDetector(
+              child: Container(
+                child: Text(
+                    "Ligar",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20
+                  ),
+                ),
+                padding: EdgeInsets.all(16),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.circular(30)
+                ),
+              ),
+              onTap: (){
+                _ligarTelefone(_anuncio.telefone);
+              },
+            ),
+          )
+
         ],
       ),
     );
