adb02d2f458586fa0a710c9e9d7e7aa088a10bdc
Marcos Vinicius Sousa do Rosario <55248963+Marcos-Vinicius-Sousa@users.noreply.github.com>
2021-10-06 21:33:04 -0300

    acrescentando um conteudo na tela sobre

--- x_change/lib/views/Sobre.dart
+++ x_change/lib/views/Sobre.dart
@@ -13,12 +13,39 @@ class _SobreState extends State<Sobre> {
         appBar: AppBar(
             title: Text("Sobre"),
             centerTitle: true,
-            backgroundColor: Colors.indigo
-        ),
+            backgroundColor: Colors.indigo),
         drawer: MenuLateral(),
-        body: Center(
-          child: Text("Conheça xChange"),
-        )
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(130,30,100,20),
+                      child: Text(
+                        "XChange",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.deepOrange,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text(
+                      "Xchange é uma aplicação voltada para a troca de serviços e produtos. Baseada em maneiras de clientes ofertarem e buscarem produtos que querem, ou serviços que precisam ser feitos. O intuito do Xchange é focar inteiramente em trocas, sem que seu usuário precise ter qualquer tipo de gasto quando utilizar algo encontrado aqui. Este serviço pode trazer maneiras econômicas para alguém ter o que precisa e também ajudar outra pessoa que busca algo que ela pode oferecer",
+                      style: TextStyle(
+                        fontSize: 25
+                      ),),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
